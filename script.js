5f06795c8955997378f240437fcc77e065f5b93d
M.Hossien <MHossien29782@gmail.com>
2023-03-05 01:41:01 -0800

    chang function TunrUp

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var alphbet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var router1 = ["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"];
 var router2 = ["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"];
 var router3 = ["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"];
-function TurnUp(array) {
-    let item = array.pop();
-    array.unshift(item);
+function TurnUp(router) {
+    let item = router.pop();
+    router.unshift(item);
 }
 function TurnDown(router) {
     let item = router.shift();
