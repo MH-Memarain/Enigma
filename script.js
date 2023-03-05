307c7ce1601845c644a3d9e7fb89ff8d624fa0d4
M.Hossien <MHossien29782@gmail.com>
2023-03-05 01:14:51 -0800

    creat 3 routers and change function TurnUp , TurnDown

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var alphbet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];                        
-alert(alphbet)
-function TurnUp(){
-    let item = alphbet.pop();    
-    alphbet.unshift(item);
+var alphbet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var router1 = ["E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"];
+var router2 = ["A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"];
+var router3 = ["B", "D", "F", "H", "J", "L", "C", "P", "R", "T", "X", "V", "Z", "N", "Y", "E", "I", "W", "G", "A", "K", "M", "U", "S", "Q", "O"];
+function TurnUp(array) {
+    let item = array.pop();
+    array.unshift(item);
 }
-function TurnDown(){
-    let item = alphbet.shift();
-    alphbet.push(item);
+function TurnDown(router) {
+    let item = router.shift();
+    router.push(item);
 }
